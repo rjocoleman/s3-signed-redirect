43b6d7200463bc61e9500a6ef2753d740cd5f29c
Robert Coleman <github@robert.net.nz>
2016-10-18 13:14:10 +1300

    go fmt

--- main.go
+++ main.go
@@ -2,71 +2,71 @@ package main
 
 import (
 	"fmt"
-  "net/http"
-  "time"
-  "path"
+	"net/http"
+	"path"
+	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
-  "github.com/goware/urlx"
+	"github.com/goware/urlx"
+	"github.com/smartystreets/go-aws-auth"
 	"github.com/spf13/viper"
-  log "github.com/Sirupsen/logrus"
-  "github.com/smartystreets/go-aws-auth"
 )
 
 func main() {
-  viper.SetConfigType("yaml")
+	viper.SetConfigType("yaml")
 	viper.SetConfigName("s3-signed-redirect")
 	viper.AddConfigPath("/etc/s3-signed-redirect/")
 	viper.AddConfigPath("$HOME/.s3-signed-redirect")
-  viper.AddConfigPath(".")
+	viper.AddConfigPath(".")
 	viper.ReadInConfig()
 	viper.SetEnvPrefix("s3sr")
 	viper.AutomaticEnv()
-  viper.BindEnv("aws_access_key_id")
-  viper.BindEnv("aws_secret_access_key")
+	viper.BindEnv("aws_access_key_id")
+	viper.BindEnv("aws_secret_access_key")
 
-  viper.SetDefault("aws_access_key_id", viper.Get("aws_access_key_id"))
-  viper.SetDefault("aws_secret_access_key", viper.Get("aws_secret_access_key"))
+	viper.SetDefault("aws_access_key_id", viper.Get("aws_access_key_id"))
+	viper.SetDefault("aws_secret_access_key", viper.Get("aws_secret_access_key"))
 
-  viper.SetDefault("debug", false)
-  viper.SetDefault("address", "127.0.0.1")
-  viper.SetDefault("port", 8080)
-  viper.SetDefault("timeout", "3600s")
+	viper.SetDefault("debug", false)
+	viper.SetDefault("address", "127.0.0.1")
+	viper.SetDefault("port", 8080)
+	viper.SetDefault("timeout", "3600s")
 
-  log.SetLevel(log.DebugLevel)
-  if !viper.GetBool("debug") {
-    gin.SetMode(gin.ReleaseMode)
-    log.SetLevel(log.InfoLevel)
-  }
-  address := viper.Get("address")
+	log.SetLevel(log.DebugLevel)
+	if !viper.GetBool("debug") {
+		gin.SetMode(gin.ReleaseMode)
+		log.SetLevel(log.InfoLevel)
+	}
+	address := viper.Get("address")
 	port := viper.GetInt("port")
 
 	r := gin.Default()
-  r.GET("/:path", func(c *gin.Context) {
+	r.GET("/:path", func(c *gin.Context) {
 
-    log.Debug("Path: ", c.Request.URL.String())
-    log.Debug("S3 Endpoint: ", viper.GetString("s3_endpoint"))
+		log.Debug("Path: ", c.Request.URL.String())
+		log.Debug("S3 Endpoint: ", viper.GetString("s3_endpoint"))
 
-    url, _ := urlx.Parse(viper.GetString("s3_endpoint"))
-    url.Path = path.Join(viper.GetString("s3_bucket"), c.Request.URL.String())
-    log.Debug("New URL: ", url.String())
+		url, _ := urlx.Parse(viper.GetString("s3_endpoint"))
+		url.Path = path.Join(viper.GetString("s3_bucket"), c.Request.URL.String())
+		log.Debug("New URL: ", url.String())
 
-    expireTime := time.Now().Add(viper.GetDuration("timeout"))
-    req, err := http.NewRequest("GET", url.String(), nil)
-    if err != nil {
-      c.String(http.StatusBadRequest, err.Error())
-      c.Abort()
-      log.WithError(err)
-      return
-    }
+		expireTime := time.Now().Add(viper.GetDuration("timeout"))
+		req, err := http.NewRequest("GET", url.String(), nil)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			c.Abort()
+			log.WithError(err)
+			return
+		}
 
-    awsauth.SignS3Url(req, expireTime, awsauth.Credentials{
-      AccessKeyID: viper.GetString("aws_access_key_id"),
-      SecretAccessKey: viper.GetString("aws_secret_access_key"),
-    })
+		awsauth.SignS3Url(req, expireTime, awsauth.Credentials{
+			AccessKeyID:     viper.GetString("aws_access_key_id"),
+			SecretAccessKey: viper.GetString("aws_secret_access_key"),
+		})
 
-    log.Debug("Redirect: ", req.URL.String())
-    c.Redirect(http.StatusFound, req.URL.String())
+		log.Debug("Redirect: ", req.URL.String())
+		c.Redirect(http.StatusFound, req.URL.String())
 	})
 	r.Run(fmt.Sprintf("%s:%d", address, port))
 }
